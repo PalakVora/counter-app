cffd973ebf8bb3c04e136f19730da9696f7be99d
Palak Vora <palak.vora19@gmail.com>
2020-07-15 01:26:02 +0530

    navbar child to parent app+counter/counters child

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,47 +2,18 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters  extends Component {
-    state = { 
-        counters:[
-            { id: 1, value:4},
-            { id: 2, value:0},
-            { id: 3, value:0},
-            { id: 4, value:0}
-        ]
-     };
-
-     handleIncrement=(counter)=>{
-        const counters=[...this.state.counters];
-        const index=counters.indexOf(counter);
-        counters[index]={...counter};
-        counters[index].value++;
-        this.setState({counters});
-     };
-
-     handleDecrement=(counterID)=>{
-        const counters=this.state.counters.filter(c=>c.id!==counterID);
-        this.setState({counters});
-     };
-     handleReset=(counterID)=>{
-        const counters= this.state.counters.map(c=>{
-            c.value=0;
-            return c;
-        });
-        this.setState({counters});
-    }
     render() { 
         return (
             <div>
                 <button
                     className={"btn btn-primary btn-small m-2"}
-                    onClick={this.handleReset}
+                    onClick={this.props.onReset}
                 >Reset</button>
-                {this.state.counters.map(counter=>
+                {this.props.counters.map(counter=>
                 <Counter 
                     key={counter.id} 
-                    onIncrement={this.handleIncrement} 
-                    onDelete={this.handleDecrement} 
-                    onReset={this.handleReset} 
+                    onIncrement={this.props.onIncrement} 
+                    onDelete={this.props.onDelete}      //raises an onDelete event and bubbling to parent; parent will handle the event
                     counter={counter}
                 >
                     
